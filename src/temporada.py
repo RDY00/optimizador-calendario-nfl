fd353c5fc2b76e10b59a0cffbd1d6133249f86f2
Emiliano2123 <53309367+Emiliano2123@users.noreply.github.com>
2023-05-21 08:05:22 -0600

    todo en una funcion

--- src/temporada.py
+++ src/temporada.py
@@ -63,15 +63,58 @@ class TemporadaNFL:
     """
     with open(archivo) as data:
       info = data.readlines()
-    i, p = get_partidos(info,0)
-    i, e = get_estadios(info,i)
-    i, q = get_equipos(info,i)
+    p = []
+    e = []
+    q = []
+    indice = 0
+    for i in range(len(info)):
+      if(info[i] == "\n"):
+        indice = i + 1
+        break
+      s = info[i].split()
+      partido = {
+        "id" : int(s[0]),
+        "local" : int(s[1]),
+        "visitante" : int(s[2]),
+        "estadio" : int(s[3]),
+        "interes" : int(s[4])
+      }
+      p.append(partido)
+      
+    for i in range(indice, len(info)):
+      if(info[i] == "\n"):
+        indice = i + 1
+        break
+      s = info[i].split()
+      estadio = {
+        "id" : int(s[0]),
+        "huso" : s[1]
+      }
+      e.append(estadio)
+    
+    for i in range(indice, len(info), 2):
+      if(info[i] == "\n"):
+        indice = i + 1
+        break
+      s = info[i].split()
+      lista = list(map(int, info[i+1].split()))
+      equipo = {
+        "id" : int(s[0]),
+        "nombre" : s[1],
+        "conferencia" : s[2],
+        "division" : s[3],
+        "bye_pasado" : s[4],
+        "3_consecutivos" : s[5],
+        "oponentes" : lista
+      }
+      q.append(equipo)
+    
     s = info[i].split()
     partidos = tuple(p)
     estadios = tuple(e)
     equipos = tuple(q)
     thanksgiving = int(s[0])
-    navidad = (s[1], s[2])
+    navidad = (int(s[1]), s[2])
     # return [partidos, estadios, equipos, thanksgiving, navidad]
     num_semanas = 18
     return cls(num_semana, equipos, partidos, estadios, navidad, thanksgiving)

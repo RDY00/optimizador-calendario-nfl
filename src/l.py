40c7e611e01afc070bac4073629276cc1658f5aa
RDY00 <thescrocherstakss02@gmail.com>
2023-06-05 00:38:18 -0600

    Codigo para pasar de xlsx a txt

--- src/l.py
+++ src/l.py
@@ -6,8 +6,9 @@ from temporada import TemporadaNFL
 from evaluacion.evaluacion2023 import EvaluacionNFL2023
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-with open("data/ejecucuion_3.json") as f:
+with open(sys.argv[1]) as f:
   datos = json.load(f)
 
 datos["solucion"] = np.array(datos["solucion"], dtype=int).reshape((32,18,2))
@@ -16,7 +17,8 @@ print("Solución:\n")
 print(datos["solucion"][:,:,0],end="\n\n")
 print(datos["solucion"][:,:,1])
 
-elv = EvaluacionNFL2023(TemporadaNFL.leer_archivo("../data/temporada2023.txt"))
+temporada = TemporadaNFL.leer_archivo("../data/temporada2023.txt")
+elv = EvaluacionNFL2023(temporada)
 
 print("\nOptimo:", elv.max_eval)
 print("\nEvaluación:", datos["evaluacion"])
@@ -24,10 +26,13 @@ print("\nEvaluación:", datos["evaluacion"])
 print("Análisis:\n")
 print(elv.analiza_solucion(datos["solucion"]))
 
-fig, ax = plt.subplots()
-ax.plot(datos["optimos"])
-ax.set_title("Evaluación del valor óptimo")
-ax.set_ylabel("Evaluación")
-ax.set_xlabel("Generación")
-fig.savefig("grafia.png", bbox_inches="tight")
+with open(sys.argv[2], "a") as f:
+  temporada.guardar_solucion(datos["solucion"])
+
+# fig, ax = plt.subplots()
+# ax.plot(datos["optimos"])
+# ax.set_title("Evaluación del valor óptimo")
+# ax.set_ylabel("Evaluación")
+# ax.set_xlabel("Generación")
+# fig.savefig("grafia.png", bbox_inches="tight")
 
